476994b4f2e83a54fe28cd3cbb0d8c2fb1a0ea01
ppetrid <ppetrid@yawd.eu>
2013-01-21 23:31:14 +0200

    use prefetch_related to reduce db load

--- paypal/pro/admin.py
+++ paypal/pro/admin.py
@@ -9,5 +9,8 @@ class PayPalNVPAdmin(admin.ModelAdmin):
     list_filter = ('flag', 'created_at')
     search_fields = ('user__email', 'ip_address', 'flag', 'firstname', 'lastname')
 
+    def queryset(self, request):
+        return super(PayPalNVPAdmin, self).queryset(request).prefetch_related('user')
+
 
 admin.site.register(PayPalNVP, PayPalNVPAdmin)
